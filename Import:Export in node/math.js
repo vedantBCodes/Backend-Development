0de36aff69fa20122d98f57a1f638dd397765f67
vedant Yevatikar <bichkundevedant97@gmail.com>
2025-03-06 12:25:54 +0530

    import/export in node

---
+++ Import:Export in node/math.js
@@ -0,0 +1,15 @@
+const pi=3.14;
+const add=(a,b)=> {return (a+b) };
+const sub=(a,b)=> {return (a-b) };
+const mul=(a,b)=> {return (a*b) };
+const div=(a,b)=> {return (a/b) };
+
+const obj={
+    pi : pi,
+    add : add,
+    sub : sub,
+    mul : mul,
+    div : div
+}
+
+module.exports = obj;
---
+++ Import:Export in node/script.js
@@ -0,0 +1,11 @@
+const obj=require('./math');
+
+console.log(obj.add(4,5));
+
+console.log(obj.mul(4,5));
+
+console.log(obj.sub(14,5));
+
+console.log(obj.pi);
+
+
