6a0426bcf4fad2a70f3aaf072a9fd257e3fb5f9b
vedant Yevatikar <bichkundevedant97@gmail.com>
2025-07-14 11:03:27 +0530

    added delete post feature

--- Express JS/QuoraPosts/index.js
+++ Express JS/QuoraPosts/index.js
@@ -64,6 +64,12 @@ app.patch("/posts/:id",(req,res)=>{
     res.redirect("/posts");
 });
 
+app.delete("/posts/:id",(req,res)=>{
+    let { id } = req.params;
+    posts = posts.filter((p)=> id !== p.id );
+    res.redirect("/posts");
+});
+
 app.get("/posts/:id/edit",(req,res)=>{
     let {id} = req.params;
     let post = posts.find((p)=> id == p.id);
