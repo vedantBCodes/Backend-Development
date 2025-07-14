61a34346bae7774f7965351080e76f97d53d1087
vedant Yevatikar <bichkundevedant97@gmail.com>
2025-07-14 10:51:49 +0530

    added editing feature

--- Express JS/QuoraPosts/index.js
+++ Express JS/QuoraPosts/index.js
@@ -1,32 +1,33 @@
 const express = require("express");
 
 const app = express();
-
+const {v4 : uuidv4} = require('uuid');
+const methodOverride = require("method-override");
 app.set("view engine","ejs");
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
-
+app.use(methodOverride("_method"));
 app.get("/",(req,res)=>{
     res.send("This is root path");
 });
 let posts = [
     {
-        id:"1a",
+        id: uuidv4() ,
         username : "Vedant",
         content : "I Love Coding"
     },
      {
-        id:"2b",
+        id:uuidv4(),
         username : "Prithvi",
         content : "I hate Coding"
     },
     {
-        id:"3c",
+        id:uuidv4(),
         username : "Suraj",
         content : "I just got my 1st internship"
     },
     {
-        id:"4d",
+        id:uuidv4(),
         username : "Amruta",
         content : "I don't like to study"
     },
@@ -41,7 +42,8 @@ app.get("/posts/new",(req,res)=>{
 app.post("/posts", (req, res) => {
     const { username, content } = req.body;
     console.log(req.body); // ✅ Now it will show the form data
-    posts.push({ username, content });
+    let id = uuidv4();
+    posts.push({ id, username, content });
     res.redirect("/posts");
 });
 
@@ -51,6 +53,21 @@ app.get("/posts/:id",(req,res)=>{
     // console.log(post);
     res.render("showPost",{post});
     // res.send("request accepted");
+});
+
+app.patch("/posts/:id",(req,res)=>{
+    let {id} = req.params;
+    let newContent = req.body.content;
+    let post = posts.find((p)=> id == p.id );
+    post.content = newContent;
+    console.log(post);
+    res.redirect("/posts");
+});
+
+app.get("/posts/:id/edit",(req,res)=>{
+    let {id} = req.params;
+    let post = posts.find((p)=> id == p.id);
+    res.render("edit.ejs",{post});
 });
 
 app.listen(3000,()=>{
